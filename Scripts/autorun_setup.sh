68c8ebf5c0adcc760fa93984ddf124a9878515c1
Vladimir Sobolev <v.sobolev@gmail.com>
2022-09-30 14:45:55 +0300

    Update voice OFF switch

--- Scripts/autorun_setup.sh
+++ Scripts/autorun_setup.sh
@@ -7,6 +7,7 @@ export PATH=/sbin:/bin:/customer/wifi:/usr/bin:/usr/sbin
 export LD_LIBRARY_PATH=/lib:/customer/wifi/lib
 REC_STATUS=/tmp/rec_status
 VIDEOPARAM=/tmp/cardv_fifo
+FW_SETENV=/config/bin/fw_setenv
 
 #for f in /mnt/mmc/autorun_*.sh; do if [ -f "$f" ]; then source $f; fi done
 
@@ -34,6 +35,7 @@ echo "pbvolume 0" > $VIDEOPARAM
 nvconf set 0 Camera.Menu.PlaybackVolume 00
 
 echo "voice 0" > $VIDEOPARAM
+$FW_SETENV VoiceSwitch 0
 nvconf set 0 Camera.Menu.VoiceSwitch OFF
 
 echo "audiorec 0" > $VIDEOPARAM
@@ -51,13 +53,19 @@ nvconf set 0 Camera.Menu.Brightness 50
 echo "con 45" > $VIDEOPARAM
 nvconf set 0 Camera.Menu.Contrast 45
 
-echo "res 0 3840 2160 " > $VIDEOPARAM
+echo "res 0 3840 2160" > $VIDEOPARAM
 nvconf set 0 Camera.Menu.VideoRes 3840x2160P30
 
 nvconf set 0 Camera.Menu.ImageRes 3M
 
 echo "flicker 50" > $VIDEOPARAM
 nvconf set 0 Camera.Menu.Flicker 50HZ
+
+# Video Clip Time = 1MIN
+nvconf set 0 Camera.Menu.VideoClipTime 1MIN
+nvconf set 0 Camera.Menu.LoopingVideo 1MIN
+echo "loop 1" > $VIDEOPARAM
+$FW_SETENV VideoClipTime 1
 
 # 10000000 = 10mbit For RTSP streaming on rtsp://192.168.10.1/liveRTSP/av4 for front or rtsp://192.168.10.1/liveRTSP/av5 for back camera
 nvconf set 1 streamer.video.bitrate 60000000
