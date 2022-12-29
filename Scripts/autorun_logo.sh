c9a127ec1794fe699dce27f98efd33b1302738f9
Vladimir Sobolev <v.sobolev@gmail.com>
2022-12-29 09:46:13 +0200

    Tweaks

--- Scripts/autorun_logo.sh
+++ Scripts/autorun_logo.sh
@@ -6,8 +6,16 @@
 export PATH=/sbin:/bin:/customer/wifi:/usr/bin:/usr/sbin
 export LD_LIBRARY_PATH=/lib:/customer/wifi/lib
 
+# restore default logo
+#cp /customer/boot0_480x854_original.jpg /misc/boot0_480x854.jpg
+
+# setup custom logo from logo.jpg
 cp /mnt/mmc/logo.jpg /misc/boot0_480x854.jpg
 
 mkdir /mnt/mmc/Autorun_completed
 mv /mnt/mmc/logo.jpg /mnt/mmc/Autorun_completed/logo.jpg
 mv /mnt/mmc/autorun_logo.sh /mnt/mmc/Autorun_completed/autorun_logo.sh
+
+sync
+sleep 5
+reboot
