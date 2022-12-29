5efe7791b995e87b597a56639c8d360d2b5ba398
Vladimir Sobolev <v.sobolev@gmail.com>
2022-12-29 16:47:49 +0200

    New Scripts

---
+++ Scripts/autorun_ap.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+##############################
+## CGI_CMD
+##############################
+export PATH=/sbin:/bin:/customer/wifi:/usr/bin:/usr/sbin
+export LD_LIBRARY_PATH=/lib:/customer/wifi/lib
+REC_STATUS=/tmp/rec_status
+VIDEOPARAM=/tmp/cardv_fifo
+FW_SETENV=/config/bin/fw_setenv
+
+#for f in /mnt/mmc/autorun_*.sh; do if [ -f "$f" ]; then source $f; fi done
+
+SSID=70mai_A800S_cad1
+PSK=multipas
+IPADDR=192.168.1.1
+SUBNETMASK=255.255.255.0
+
+nvconf set 1 wireless.ap.ssid $SSID
+nvconf set 1 wireless.ap.wpa.psk $PSK
+nvconf set 1 wireless.apstaswitch AP
+
+sleep 1
+
+echo " Kill all process of STA Mode"
+busybox killall udhcpc
+busybox killall wpa_supplicant
+busybox killall goahead
+ifconfig wlan0 down
+rmmod cfg80211
+
+echo "Launch Wifi module AP Mode ..."
+
+insmod /customer/wifi/lib/cfg80211.ko
+sleep 1
+ifconfig wlan0 $IPADDR netmask $SUBNETMASK
+sleep 1
+/usr/sbin/udhcpd -S /customer/wifi/udhcpd-ap.conf &
+
+cp /customer/wifi/hostapd.conf /mnt/mmc/hostapd.conf
+sed -i "s/^ssid.*$/ssid=$SSID/" /mnt/mmc/hostapd.conf
+sed -i "s/^wpa_passphrase.*$/wpa_passphrase=$PSK/" /mnt/mmc/hostapd.conf
+
+hostapd /mnt/mmc/hostapd.conf -B
+source /customer/wifi/run_goahead.sh
---
+++ Scripts/autorun_sta.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+##############################
+## CGI_CMD
+##############################
+export PATH=/sbin:/bin:/customer/wifi:/usr/bin:/usr/sbin
+export LD_LIBRARY_PATH=/lib:/customer/wifi/lib
+REC_STATUS=/tmp/rec_status
+VIDEOPARAM=/tmp/cardv_fifo
+FW_SETENV=/config/bin/fw_setenv
+
+#for f in /mnt/mmc/autorun_*.sh; do if [ -f "$f" ]; then source $f; fi done
+
+SSID=wifiname
+PSK=wifipassword
+IPADDR=192.168.1.245
+SUBNETMASK=255.255.255.0
+
+echo "Kill all process of AP Mode"
+busybox killall udhcpd
+busybox killall hostapd
+busybox killall goahead
+ifconfig wlan0 down
+
+rmmod cfg80211
+
+nvconf set 1 wireless.sta.ssid $SSID
+nvconf set 1 wireless.sta.wpa.psk $PSK
+nvconf set 1 wireless.sta.ipaddr $IPADDR
+nvconf set 1 wireless.sta.subnetmask $SUBNETMASK
+nvconf set 1 wireless.apstaswitch STA
+
+printf '#ctrl_interface=/tmp/wpa_supplican\nupdate_config=1  \n\nnetwork={  \n    proto=WPA2\n    key_mgmt=WPA-PSK\n    ssid="%s"  \n    psk="%s"  \n}\n' "$SSID" "$PSK" > /mnt/mmc/wpa_supplicant.conf
+
+#cp -f /mnt/mmc/wpa_supplicant1.conf /customer/wifi/wpa_supplicant.conf
+#rm /mnt/mmc/wpa_supplicant.conf 
+
+#source /customer/wifi/apsta_switch.sh > /mnt/mmc/apsta_switch.txt
+
+#cp /customer/wifi/wpa_supplicant.conf /mnt/mmc/wpa_supplicant2.conf
+
+echo "Launch Wifi module STA Mode..."
+
+insmod /customer/wifi/lib/cfg80211.ko
+sleep 1
+
+ifconfig wlan0 up
+wpa_supplicant -Dnl80211 -iwlan0 -c /mnt/mmc/wpa_supplicant.conf -B
+udhcpc -iwlan0 -b
+#static ip
+ifconfig wlan0 $IPADDR netmask $SUBNETMASK
+
+source /customer/wifi/run_goahead.sh
