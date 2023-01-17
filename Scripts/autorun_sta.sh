c6cec6ec4da59c209443492641f9bb1b73deda1c
Vladimir Sobolev <v.sobolev@gmail.com>
2023-01-17 11:31:23 +0200

    STA mode update

--- Scripts/autorun_sta.sh
+++ Scripts/autorun_sta.sh
@@ -16,24 +16,42 @@ PSK=wifipassword
 IPADDR=192.168.1.245
 SUBNETMASK=255.255.255.0
 
+echo "Wait for boot..."
+sleep 4
+
 echo "Kill all process of AP Mode"
+busybox killall goahead
 busybox killall udhcpd
 busybox killall hostapd
-busybox killall goahead
+busybox killall wpa_supplicant
+
+nmcli nm wifi off
+#rfkill unblock wlan
+
 ifconfig wlan0 down
+
+rmmod ssv6x5x
 rmmod cfg80211
 
-nvconf set 1 wireless.sta.ssid $SSID
-nvconf set 1 wireless.sta.wpa.psk $PSK
-nvconf set 1 wireless.sta.ipaddr $IPADDR
-nvconf set 1 wireless.sta.subnetmask $SUBNETMASK
-nvconf set 1 wireless.apstaswitch STA
+#nvconf set 1 wireless.sta.ssid $SSID
+#nvconf set 1 wireless.sta.wpa.psk $PSK
+#nvconf set 1 wireless.sta.ipaddr $IPADDR
+#nvconf set 1 wireless.sta.subnetmask $SUBNETMASK
+#nvconf set 1 wireless.apstaswitch STA
 
 printf '#ctrl_interface=/tmp/wpa_supplican\nupdate_config=1  \n\nnetwork={  \n    proto=WPA2\n    key_mgmt=WPA-PSK\n    ssid="%s"  \n    psk="%s"  \n}\n' "$SSID" "$PSK" > /mnt/mmc/wpa_supplicant.conf
 
 echo "Launch Wifi module STA Mode..."
 
-insmod /customer/wifi/lib/cfg80211.ko
+if [ "`lsmod|grep cfg80211`" == "" ]; then
+        insmod /customer/wifi/lib/cfg80211.ko
+fi
+
+if [ "`lsmod|grep ssw101b_wifi_sdio`" == "" ]; then
+        insmod /customer/wifi/lib/ssv6x5x.ko
+fi
+
+
 sleep 1
 ifconfig wlan0 up
 wpa_supplicant -Dnl80211 -iwlan0 -c /mnt/mmc/wpa_supplicant.conf -B
